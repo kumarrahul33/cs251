f5c0b549aafe481f613507fcc274e9843d21c658
Hermione <Hermione@hogwarts.edu>
2022-08-12 11:13:36 +0530

    Update hash implementation

--- hashing.cpp
+++ hashing.cpp
@@ -2,9 +2,10 @@
 
 int hash_string(string s) {
     int m = 31;
+    int p = 7;
     int sum = 0;
     for (int i = 0; i < s.length(); i++) {
-        sum += s[i];
+        sum += s[i] * pow(p, i);
     }
     return sum % m;
 }
